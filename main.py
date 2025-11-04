c095719c95a77a0232eaa8db39962b6ade834a9c
Kunal Pawar <kunalkp2014@gmail.com>
2025-11-04 13:05:22 +0530

    Add headers to requests for Amazon price check

--- main.py
+++ main.py
@@ -7,9 +7,23 @@ AMAZON_ENDPOINT = os.getenv("AMAZON_ENDPOINT")
 EMAIL_ID = os.getenv("EMAIL_ID")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
+header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-GB,de;q=0.8,fr;q=0.6,en;q=0.4,ja;q=0.2",
+    "Dnt": "1",
+    "Priority": "u=1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Sec-Gpc": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
+}
 
 
-response = requests.get(AMAZON_ENDPOINT)
+response = requests.get(AMAZON_ENDPOINT, headers=header)
 response_data = response.text
 
 soup = BeautifulSoup(response_data, 'html.parser')
